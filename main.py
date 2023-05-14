0273c70e89cad6719605719d3d8861e71e013363
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-05-14 09:12:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import requests
-import bs4
-
+from bs4 import BeautifulSoup
 from telegram.ext import Updater, CommandHandler
 
 
 # Define your bot token here
-BOT_TOKEN = '6201307785:AAGZrQXOhM7m70Gcne4mTBuInrmc_Ta5dyY'
+BOT_TOKEN = '6201307785:AAGZrQXOhM7m70Gcne4mTBuInrmc_Ta5dyY
 
 
 def anime_search(update, context):
     """Searches for anime and returns a download link."""
     query = ' '.join(context.args)
-    url = "https://www.zoro.to/search?q=" + query
+    url = "https://www.gogoanime.io/search.html?keyword=" + query
     response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.content, "html.parser")
-    results = soup.find_all("div", class_="col-sm-6 col-md-4 col-lg-3")
+    soup = BeautifulSoup(response.content, "html.parser")
+    results = soup.find_all("div", class_="last_episodes loaddub")
     for result in results:
-        title = result.find("a", class_="title").text
-        link = result.find("a", class_="title").get("href")
-        context.bot.send_message(chat_id=update.effective_chat.id, text=title + " - " + link)
+        title = result.find("p", class_="name").text
+        link = result.find("a").get("href")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"{title} - {link}")
 
 
 def start(update, context):
