1f9a1b9d4ac45a67cc3d8b4c4448e5337ad5dd78
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-05-14 08:59:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 
-from telegram import Update, Dispatcher, CommandHandler
+from telegram.ext import Updater, CommandHandler
 
+# Get your API ID and API hash from https://api.telegram.org/botfather
+API_ID = 7211896
+API_HASH = 'd22a4d25860c6673209ea07dc194857a'
 
-def get_anime_link(anime_name):
-  """Gets the download link for an anime from Zoro.to."""
-  url = f"https://www.zoro.to/search?q={anime_name}"
-  response = requests.get(url)
-  soup = BeautifulSoup(response.content, "html.parser")
-  links = soup.find_all("a", class_="btn btn-success btn-block")
-  return links[0]["href"]
+# Get your bot token from https://t.me/botfather
+BOT_TOKEN = '6201307785:AAGZrQXOhM7m70Gcne4mTBuInrmc_Ta5dyY'
 
+# Create the updater
+updater = Updater(token=BOT_TOKEN, use_context=True)
 
-def start(update: Update, context: Dispatcher):
-  """Sends a welcome message to the user."""
-  update.message.reply_text("Welcome to the anime bot! You can search for anime by typing /search <anime_name>.")
+# Define a command handler for the /search command
+@updater.command(command='search')
+def search(update, context):
+    # Get the anime name from the user
+    query = update.message.text
 
+    # Scrape the download links from 9anime.to
+    url = 'https://9anime.to/search?q=' + query
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')
 
-def search(update: Update, context: Dispatcher):
-  """Searches for an anime and sends the download link to the user."""
-  anime_name = update.message.text.split()[1]
-  link = get_anime_link(anime_name)
-  update.message.reply_text(link)
+    # Find the download links
+    download_links = soup.find_all('a', class_='btn btn-success btn-sm')
 
+    # Send the download links to the user
+    for download_link in download_links:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=download_link['href'])
 
-if __name__ == "__main__":
-  # Create the updater and dispatcher
-  updater = Updater("6201307785:AAGZrQXOhM7m70Gcne4mTBuInrmc_Ta5dyY")
-  dispatcher = updater.dispatcher
+# Start the bot
+updater.start_polling()
 
-  # Add the handlers
-  dispatcher.add_handler(CommandHandler("start", start))
-  dispatcher.add_handler(CommandHandler("search", search))
-
-  # Start the bot
-  updater.start_polling()
-  updater.idle()
+# Run the bot until you press Ctrl+C
+updater.idle()
