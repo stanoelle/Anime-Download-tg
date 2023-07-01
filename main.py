b01f716ccf72586b71597496ed5be65498051cfb
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-07-01 17:34:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,28 @@ import telebot
 import requests
 from bs4 import BeautifulSoup
 
+# Create a new Telegram bot
 bot = telebot.TeleBot('6201307785:AAG0gB_uqvs9_konxBuLouEH9fwvwIQdzsM')
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Hello! I am a movie download bot. Just send me the name of the movie you want to download and I will provide direct download links.')
+# Define the command to search for movies
+@bot.message_handler(commands=['search'])
+def search(message):
+    # Get the movie title from the user's message
+    title = message.text.replace('/search ', '')
 
-@bot.message_handler(func=lambda msg: msg.text is not None) 
-def send_movie_links(message):
-    movie_name = message.text
-    sites = ['yts', 'eztv', 'rarbg']
-    for site in sites:
-        search_url = f'https://{site}.ag/search/{movie_name}/'
-        response = requests.get(search_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Find all links to torrent files or direct downloads
-        links = soup.find_all('a', {'href': re.compile('.*\.(torrent|mp4|mkv)')}) 
-        
-        # Get first 5 links and send to user
-        send_links = ''
-        for i, link in enumerate(links):
-            if i == 5: 
-                break
-            send_links += link.get('href') + '\n'
-        if send_links:
-            bot.reply_to(message, send_links)
-            break
-bot.infinity_polling()
+    # Search for the movie on the download website
+    response = requests.get(f'https://yts.mx/browse-movies/{title}/all/all/0/latest')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    movie_links = soup.find_all('a', {'class': 'browse-movie-title'})
+
+    # If there are no search results, let the user know
+    if not movie_links:
+        bot.reply_to(message, 'Sorry, no results found. Please try again with a different search term.')
+        return
+
+    # Otherwise, send the user the first movie's download link
+    movie_link = movie_links[0]['href']
+    bot.reply_to(message, f'Here is the download link for {title}: {movie_link}')
+
+# Start the bot
+bot.polling()
