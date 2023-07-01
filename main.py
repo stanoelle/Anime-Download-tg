404c8a738c68d890e3eb8d5c91c245eb0cb0daac
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-07-01 16:53:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 import requests
 from bs4 import BeautifulSoup
 
-bot = telebot.TeleBot('YOUR_BOT_TOKEN')
+bot = telebot.TeleBot('6201307785:AAG0gB_uqvs9_konxBuLouEH9fwvwIQdzsM')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -11,18 +11,21 @@ def send_welcome(message):
 @bot.message_handler(func=lambda msg: msg.text is not None) 
 def send_movie_links(message):
     movie_name = message.text
-    search_queries = [
-        f'index of {movie_name} mkv',
-        f'index of {movie_name} mp4'
-    ]
-    for query in search_queries:
-        response = requests.get(f'https://google.com/search?q={query}')
+    sites = ['yts', 'eztv', 'rarbg']
+    for site in sites:
+        search_url = f'https://{site}.ag/search/{movie_name}/'
+        response = requests.get(search_url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        links = soup.find_all('a')
+        
+        # Find all links to torrent files or direct downloads
+        links = soup.find_all('a', {'href': re.compile('.*\.(torrent|mp4|mkv)')}) 
+        
+        # Get first 5 links and send to user
         send_links = ''
-        for link in links:
-            if any(name in link.get('href') for name in ['mp4', 'mkv']):
-                send_links += link.get('href') + '\n'
+        for i, link in enumerate(links):
+            if i == 5: 
+                break
+            send_links += link.get('href') + '\n'
         if send_links:
             bot.reply_to(message, send_links)
             break
