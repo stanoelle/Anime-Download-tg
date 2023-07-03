ee3344c90f0506e493f6ed15257837f24a78a1b9
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-07-03 23:28:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ bot = telebot.TeleBot("6201307785:AAG0gB_uqvs9_konxBuLouEH9fwvwIQdzsM")
 def search_movies(query):
     with Client("BQAFUYDEHtMiZWocKu5762avgI1rUfRqEXdSRnCjItXqSpzDifoGSWhUwQG3XU8pElqA0muKkMSap7L4VIMlI3nneIEGm_woktY4z8u9ghKfNkBjUEaxwEoBUlTADaWfnJ_QT9JV1zrUSJ2Vb0035JI7xifKPD7faa1o-EJSI5lT5KR-QVijaaTXYVoV-1d3RDchKhAp7xUENuT22Yn4e0Uy5mofftxWPzpiFVoHKmmZy17f8Jxtg_sgVFsQZBBV0_RXzUf2HKvX9Ri2BEa9P11iuF_xClfe-d_g7tBHiTjhqIgbhg3ja4SI3rZ48ClzT21sLa1lPrf8dwwUIg1JNQqPAAAAAXauqbEA", api_id=26331302, api_hash='5d7bbff0c04b119735e4e14bdb402e69') as app:
         # Join the channel
-        app.join_chat("https://t.me/moviessfreeee")
+        app.join_chat("moviessfreeee")
         # Search for movies in the channel
-        messages = app.get_chat_history("https://t.me/moviessfreeee", filter='video', limit=10)
+        messages = app.get_chat_history("moviessfreeee", filter='video', limit=10)
         for message in messages:
             if query.lower() in message.caption.lower():
                 # Return the file URL
