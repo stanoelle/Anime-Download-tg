6ba468ff007e906d0812e24f9d51c925046be97c
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-07-03 15:59:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def search_movie(message):
     movie_name = ' '.join(message.text.split()[1:])
     with client:
         for dialog in client.iter_dialogs():
-            dialog.chat.title == 'YOUR_CHANNEL_NAME':
+            dialog.chat.title == 'moviessfreeee':
                 for message in client.search_messages(dialog.chat.id, query=movie_name):
                     bot.send_document(message.chat.id, message.document.file_id)
 bot.polling()
